a91ddc08dffd43f9d898087b37bdc4c248675792
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-10-14 02:51:19 +0200

    Fabrication d'une page de personnalisation des informations (mise en pause)

--- appli-react/src/App.js
+++ appli-react/src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
+import { useState } from "react";
+
 import './styles/index.css';
 import Home from './pages/Home';
-
+import Personnalisation from './pages/Personnalisation';
 function App() {
+
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/personnalisation" element={<Personnalisation />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ appli-react/src/pages/Personnalisation.js
@@ -0,0 +1,25 @@
+
+function Personnalisation({ datas }) {
+
+    return (datas &&
+        <form>
+            {/* {
+                datas.savoir_faire.map((data, i) =>
+                    <>
+                        <input type="checkbox" name="savoir_faire" id="case1" />
+                        <label for="case1">Truc 1</label>
+                        <br />
+                    </>
+                )
+            }
+
+
+            <input type="checkbox" name="case2" id="case2" />
+            <label for="case1">Truc 2</label>
+            <br />
+            <input type="checkbox" name="case3" id="case3" />
+            <label for="case1">Truc 3</label> */}
+        </form>
+    );
+}
+export default Personnalisation;
